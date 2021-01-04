4e6bc6594efc3b495ac2db0f202372994afa390c
Sahil Athia <sahilathia918@gmail.com>
2021-01-04 12:56:45 -0500

    move game logic to class for OOP

--- game.rb
+++ game.rb
@@ -1,8 +1,10 @@
 class Game
   attr_accessor :is_game_over
-  def initialize
+  def initialize(p1, p2)
     @turn = "Player 1"
     @is_game_over = false
+    @player1 = p1
+    @player2 = p2
   end
   
   def question(numbers)
@@ -31,5 +33,43 @@ class Game
     puts "\n----- GAME OVER -----"
     puts "#{winning_player} wins with #{lives}/3 lives remaining"
     puts "goodbye"
+  end
+
+  def start 
+    while !self.is_game_over
+      self.reset
+      numbers = [rand(20), rand(20)]
+      self.question(numbers)
+      answer = gets.chomp
+      
+      if self.check_answer(answer, numbers) == false
+        @player1.lives = @player1.lives - 1
+      end
+    
+      if @player1.lives == 0
+        self.game_over("Player 2", player2.lives)
+        break
+      end
+    
+      puts "P1: #{@player1.lives}/3 P2: #{@player2.lives}/3"
+      puts " \n ----- New Turn ----- \n "
+    
+      self.toggle_player
+      numbers = [rand(20), rand(20)]
+      self.question(numbers)
+      answer = gets.chomp
+      
+      if self.check_answer(answer, numbers) == false
+        @player2.lives = @player2.lives - 1
+      end
+    
+      if @player2.lives == 0
+        self.game_over("Player 1", @player1.lives)
+        break
+      end
+    
+      puts "P1: #{@player1.lives}/3 P2: #{@player2.lives}/3"
+      puts "\n ----- New Turn ----- \n "
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -3,41 +3,7 @@ require "./player"
 
 player1 = Player.new
 player2 = Player.new
-game = Game.new
+game = Game.new(player1, player2)
+game.start
 
-while !game.is_game_over
-  game.reset
-  numbers = [rand(20), rand(20)]
-  game.question(numbers)
-  answer = gets.chomp
-  
-  if game.check_answer(answer, numbers) == false
-    player1.lives = player1.lives - 1
-  end
-
-  if player1.lives == 0
-    game.game_over("Player 2", player2.lives)
-    break
-  end
-
-  puts "P1: #{player1.lives}/3 P2: #{player2.lives}/3"
-  puts " \n ----- New Turn ----- \n "
-
-  game.toggle_player
-  numbers = [rand(20), rand(20)]
-  game.question(numbers)
-  answer = gets.chomp
-  
-  if game.check_answer(answer, numbers) == false
-    player2.lives = player2.lives - 1
-  end
-
-  if player2.lives == 0
-    game.game_over("Player 1", player1.lives)
-    break
-  end
-
-  puts "P1: #{player1.lives}/3 P2: #{player2.lives}/3"
-  puts "\n ----- New Turn ----- \n "
-end
 
